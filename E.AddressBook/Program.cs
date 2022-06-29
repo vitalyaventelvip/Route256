841196b95916390c2ce5c9e75a8172a781d83a7c
Vitaly Trukhanov <vitalyaventel@gmail.com>
2022-06-30 02:01:45 +0300

    Finished contests E

---
+++ E.AddressBook/Program.cs
@@ -0,0 +1,45 @@
+﻿var testCaseCount = int.Parse(Console.ReadLine());
+
+while(testCaseCount > 0)
+{
+    var abonentCount = int.Parse(Console.ReadLine());
+    var abonentCollection = new Dictionary<string, List<string>>();
+
+    for (int i = 0; i < abonentCount; i++)
+    {
+        var journalRecordCollection = Console.ReadLine().Split(' ').ToArray();
+        var abonentName = journalRecordCollection[0];
+        var abonentPhone = journalRecordCollection[1];
+
+        if (abonentCollection.TryGetValue(abonentName, out var phoneCollection))
+        {
+            if (phoneCollection.Contains(abonentPhone))
+            {
+                phoneCollection.Remove(abonentPhone);
+            }
+
+            if (phoneCollection.Count == 5)
+            {
+                phoneCollection.RemoveAt(0);
+            }
+
+            phoneCollection.Add(abonentPhone);
+
+            abonentCollection[abonentName] = phoneCollection;
+        }
+        else
+        {
+            abonentCollection.Add(abonentName, new List<string>() { abonentPhone });
+        }
+    }
+
+    foreach (var abonent in abonentCollection.OrderBy(p => p.Key))
+    {
+        abonent.Value.Reverse();
+
+        Console.WriteLine("{0}: {1} {2}", abonent.Key, abonent.Value.Count, string.Join(' ', abonent.Value));
+    }
+    Console.WriteLine();
+
+    testCaseCount--;
+}
